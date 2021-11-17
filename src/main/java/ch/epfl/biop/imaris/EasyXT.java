54ee37da7446487b4433431fecac83eab126521c
Laurent Guerard <laurent.guerard@unibas.ch>
2021-11-17 11:51:01 +0100

    Start trying to fix 2D surfaces

--- src/main/java/ch/epfl/biop/imaris/EasyXT.java
+++ src/main/java/ch/epfl/biop/imaris/EasyXT.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -990,7 +990,8 @@ public class EasyXT {
 
             imp.setStack(stack);
             imp.setCalibration(cal);
-            imp = HyperStackConverter.toHyperStack(imp, nc, nz, nt);
+            if (nc != 1 && nz != 1 && nt != 1)
+                imp = HyperStackConverter.toHyperStack(imp, nc, nz, nt);
 
             // Set LookUpTables
             if (imp instanceof CompositeImage) {
